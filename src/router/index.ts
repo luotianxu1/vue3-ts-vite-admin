bea8ca7f7ee685f0917890f05c6564aa0a3455ab
罗天旭 <1265062404@qq.com>
2023-04-01 10:57:48 +0800

    fix(custom): bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ const router = createRouter({
 		{
 			path: "/login",
 			name: "login",
-			component: () => import("@views/login/Login.vue")
+			component: () => import("@/views/login/Login.vue")
 		}
 	]
 })
